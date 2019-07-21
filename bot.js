895c677862bfdcf843d9fa07f8a1664d1112eced
phobiac01 <phobiac01@gmail.com>
2019-07-21 01:07:16 -0700

    yay

--- bot.js
+++ bot.js
@@ -3,21 +3,17 @@ var SlackBot = require('slackbots');
 // create a bot
 var bot = new SlackBot({
     token: 'xoxb-653557557380-656426146838-Ba5CUBV3DTQR3oeKcRUti6Pr',
-    name: 'heatsync-dev'
+    name: 'hsl-slackbot'
 });
 
 bot.on('start', function() {
     var params = {
-        icon_emoji: ':cat:'
+        icon_emoji: ':ok_hand:'
     };
 
     // define channel, where bot exist. You can adjust it there https://my.slack.com/services
-    bot.postMessageToChannel('general', 'meow!', params);
+    bot.postMessageToChannel('hsl-dev-chan', '>>> Bot successfully started.', params);
 
     // define existing username instead of 'user_name'
-    bot.postMessageToUser('user_name', 'meow!', params);
-
-    // If you add a 'slackbot' property,
-    // you will post to another user's slackbot channel instead of a direct message
-    bot.postMessageToUser('user_name', 'meow!', { 'slackbot': true, icon_emoji: ':cat:' });
+    bot.postMessageToUser('phobiac01', 'FUCKKKKK! :ok_hand: :ok_hand: :ok_hand:', params);
 });
