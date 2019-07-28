e454ab126e28dad19a3c226f14e0e52be471c00f
phobiac01 <phobiac01@gmail.com>
2019-07-28 01:08:21 -0700

    Added weather
    Version control ftw

--- index.js
+++ index.js
@@ -2,69 +2,10 @@ const { RTMClient } = require('@slack/rtm-api');
 const token = 'xoxb-653557557380-656426146838-Ba5CUBV3DTQR3oeKcRUti6Pr';
 const rtm = new RTMClient(token);
 const date = require('date-and-time');
-const weather = require('weather.js');
+const weather = require('weather-js');
 
 var invoker = 'hsl.';
 
-
-function botLogic(event, message, arg, command, sender){
-  switch(command){
-    case 'ping':
-      rtm.sendMessage('>>> Pong!', event.channel);
-      break;
-    case 'hi':
-    case 'hello':
-    case 'yo':
-    case 'hey':
-      rtm.sendMessage('Hey there! :grin:', event.channel);
-      break;
-    case 'add':
-      try{
-        var sum = 0;
-        for(var i = 0; i < arg.length; i++){
-          if(isNaN(parseInt(arg[i]))) throw "NaN";
-          sum = sum + parseInt(arg[i]);
-        }//for close
-        rtm.sendMessage('The result is ' + sum, event.channel);
-      } catch(err) {
-        rtm.sendMessage('>>> ERR: Please only use numbers', event.channel);
-      }
-      break;
-    case 'sub':
-      try{
-        var sum = 0;
-        for(var i = 0; i < arg.length; i++){
-          if(isNaN(parseInt(arg[i]))) throw "NaN";
-          sum = sum - parseInt(arg[i]);
-        }//for close
-        rtm.sendMessage('The result is ' + sum, event.channel);
-      } catch(err) {
-        rtm.sendMessage('>>> ERR: Please only use numbers', event.channel);
-      }
-      break;
-    case 'weather':
-      var yay = 'boo';
-      break;
-    case 'time':
-      const now = date.format(new Date(), 'hh:mm A');
-      rtm.sendMessage('>>> The time is ' + now, event.channel);
-      break;
-    case 'date':
-      const today = date.format(new Date(), 'ddd, MMM DD');
-      rtm.sendMessage('>>> It is ' + today, event.channel);
-      break;
-
-
-    /*=======================================================================*/
-    case 'shutdown':
-      rtm.sendMessage('>>> HSL Slackbot is shutting down...' , event.channel);
-      process.exit(0);
-      break;
-    default:
-      return "err";
-      break;
-    } //switch close
-}
 
 rtm.on('message', (event) => {
   //rtm.sendMessage(`Welcome to the channel, <@${event.user}>`, event.channel);
@@ -79,8 +20,89 @@ rtm.on('message', (event) => {
       var command = arg.shift(); //extracts the command
       var sender = message.author;
 
-      var run = botLogic(event, message, arg, command, sender, date);
-      if (run == "err") throw "-ERR: Not a valid command";
+
+      switch(command){
+        case 'ping':
+          rtm.sendMessage('>>> Pong!', event.channel);
+          break;
+        case 'hi':
+        case 'hello':
+        case 'yo':
+        case 'hey':
+          rtm.sendMessage('Hey there! :grin:', event.channel);
+          break;
+        case 'add':
+          try{
+            var sum = 0;
+            for(var i = 0; i < arg.length; i++){
+              if(isNaN(parseInt(arg[i]))) throw "NaN";
+              sum = sum + parseInt(arg[i]);
+            }//for close
+            rtm.sendMessage('The result is ' + sum, event.channel);
+          } catch(err) {
+            rtm.sendMessage('>>> ERR: Please only use numbers', event.channel);
+          }
+          break;
+        case 'sub':
+          try{
+            var sum = 0;
+            for(var i = 0; i < arg.length; i++){
+              if(isNaN(parseInt(arg[i]))) throw "NaN";
+              sum = sum - parseInt(arg[i]);
+            }//for close
+            rtm.sendMessage('The result is ' + sum, event.channel);
+          } catch(err) {
+            rtm.sendMessage('>>> ERR: Please only use numbers', event.channel);
+          }
+          break;
+        case 'weather':
+          try {
+            weather.find({search: arg.join(" "), degreeType: "F"}, function(err, result) {
+              if (err) rtm.sendMessage(err, event.channel);
+
+              var current = result[0].current;
+
+              var wEmoji = '';
+              switch (current.skytext.toLowerCase()){
+                case 'clear':
+                  wEmoji = ':sunny:';
+                  break;
+                case 'mostly clear':
+                  wEmoji = ':sun_small_cloud:'
+                  break;
+                case 'cloudy':
+                  wEmoji = ':cloud:';
+                  break;
+                default:
+                  wEmoji = ':grey_question:'
+                  break;
+              }
+
+              rtm.sendMessage(wEmoji + '  ' + current.skytext + '\nTemp: ' + current.temperature + ' F\nFeels like: ' + current.feelslike + '\nWinds: ' + current.windspeed, event.channel);
+            });
+          } catch (err){
+            console.log(err);
+          }
+          break;
+        case 'time':
+          const now = date.format(new Date(), 'hh:mm A');
+          rtm.sendMessage('>>> The time is ' + now, event.channel);
+          break;
+        case 'date':
+          const today = date.format(new Date(), 'ddd, MMM DD');
+          rtm.sendMessage('>>> It is ' + today, event.channel);
+          break;
+
+
+        /*=======================================================================*/
+        case 'shutdown':
+          rtm.sendMessage('>>> HSL Slackbot is shutting down...' , event.channel);
+          process.exit(0);
+          break;
+        default:
+          throw "-ERR: Not a valid command";
+          break;
+        } //switch close
 
     }//'if invoker present' close
   } catch(err) {
